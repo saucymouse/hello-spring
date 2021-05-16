6842fbf0699c936e1eac05541ba2b1c82e25eb25
saucymouse <will.staton@gmail.com>
2021-05-16 19:12:56 -0400

    can't get createMessage to work inside RequestMapping controller

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,33 +8,52 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("hello")
 public class HelloController {
 
-    @GetMapping//method handles GET requests
-    //tells Spring Boot that this method returns plain text HTTP response
-    public String hello() {
-        return "Hello, Spring";
-    }
-
     // Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
-
-    // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return String.format("Hello, %s!", name);
+    public String helloWithQueryParam(@RequestParam String language, String name) {
+//        String output = createMessage(language, name);
+        String output = language + " " + name;
+        return output;
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" + // submit a request to /hello
+                "<form action='/hello' method='post'>" + // submit a request to /hello
                 "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='esperanto'>Esperanto</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
+    public static String createMessage(String language, String name) {
+        String greeting = "";
+        if (language == "english") {
+            greeting = "Hello, ";
+        }
+        if (language == "french") {
+            greeting = "Bonjour, ";
+        }
+        if (language == "german") {
+            greeting = "Hallo, ";
+        }
+        if (language == "spanish") {
+            greeting = "Hola, ";
+        }
+        if (language == "esperanto") {
+            greeting = "Saluton, ";
+        }
+        return greeting + name;
+    }
+
+
 }
--- src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
@@ -2,12 +2,28 @@ package org.launchcode.hellospring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.launchcode.hellospring.controllers.HelloController;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.launchcode.hellospring.controllers.HelloController.createMessage;
 
 @SpringBootTest
 class HelloSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void emptyTest() {
+		assertTrue(true);
+	}
+
+	@Test
+	void createMessageWorks() {
+		String expected = "Saluton, Will";
+		String actual = createMessage("esperanto", "Will");
+		assertTrue(expected.equals(actual));
 	}
 
 }
